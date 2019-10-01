0ccf69ee701bfc65ebb5faa881bc42d3bc1f4697
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-01 09:55:22 +0200

    Creazione processi

--- AppProcessi/Program.cs
+++ AppProcessi/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,26 @@ namespace AppProcessi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
-            Console.WriteLine();
+            Console.WriteLine("Esercitazione sulla gestione dei processi");
+            Process.Start("Notepad.exe");
+
+            Process.Start("Notepad.exe", @"\\dc01srv\_Condivisa\INF\4 A\Matteo Uras\Gestione_processi\AppProcessi\Hello_World.txt");
+
+            Process.Start(@"Chrome.exe", @"https://www.youtube.it");
+
+            var app = new Process();
+            app.StartInfo.FileName = @"Notepad.exe";
+            app.StartInfo.Arguments = @"\\dc01srv\_Condivisa\INF\4 A\Matteo Uras\Gestione_processi\AppProcessi\Hello_World.txt";
+            app.Start();
+            app.PriorityClass = ProcessPriorityClass.RealTime;
+
+            //app.WaitForExit();
+            Console.WriteLine("Programma terminato!");
+
+
+
+            Console.ReadLine();
+
         }
     }
 }
