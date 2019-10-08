6803eee843147b6ec1653bae4713c2d473a71763
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-08 09:08:21 +0200

    Aggiunta di commenti

--- AppProcessi/Program.cs
+++ AppProcessi/Program.cs
@@ -18,17 +18,26 @@ namespace AppProcessi
 
             Process.Start(@"Chrome.exe", @"https://www.youtube.it");
 
+            //Con questo sistema creo un processo
             var app = new Process();
+
             app.StartInfo.FileName = @"Notepad.exe";
             app.StartInfo.Arguments = @"\\dc01srv\_Condivisa\INF\4 A\Matteo Uras\Gestione_processi\AppProcessi\Hello_World.txt";
             app.Start();
             app.PriorityClass = ProcessPriorityClass.RealTime;
 
             //app.WaitForExit();
+            var Processes = Process.GetProcesses();
+            foreach (var p in Processes)
+            {
+                if(p.ProcessName=="notepad")
+                {
+                    //Prendo il processo e lo ammazzo
+                    p.Kill();
+                }
+            }
+
             Console.WriteLine("Programma terminato!");
-
-
-
             Console.ReadLine();
 
         }
